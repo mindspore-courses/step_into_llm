523b8020483d353ebe9508d80ad8348ed1e9a0ad
xingyiren <xingyiren@huawei.com>
2023-06-07 21:41:41 +0800

    add mindprompt and example

---
+++ 7.Prompt/mindprompt/prompts/manual_verbalizer.py
@@ -0,0 +1,220 @@
+# Copyright 2023 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+from typing import *
+
+import mindspore.ops as ops
+from mindspore import Tensor, Parameter
+from mindnlp.abc.transforms import PreTrainedTokenizer
+
+from mindprompt import Verbalizer
+
+
+class ManualVerbalizer(Verbalizer):
+    r"""
+    The basic manually defined verbalizer class, this class is inherited from the :obj:`Verbalizer` class.
+
+    Args:
+        tokenizer (:obj:`PreTrainedTokenizer`): The tokenizer of the current pre-trained model to point out the vocabulary.
+        classes (:obj:`List[Any]`): The classes (or labels) of the current task.
+        label_words (:obj:`Union[List[str], List[List[str]], Dict[List[str]]]`, optional): The label words that are projected by the labels.
+        prefix (:obj:`str`, optional): The prefix string of the verbalizer (used in PLMs like RoBERTa, which is sensitive to prefix space)
+        multi_token_handler (:obj:`str`, optional): The handling strategy for multiple tokens produced by the tokenizer.
+        post_log_softmax (:obj:`bool`, optional): Whether to apply log softmax post processing on label_logits. Default to True.
+    """
+
+    def __init__(self,
+                 tokenizer: PreTrainedTokenizer,
+                 classes: Optional[List] = None,
+                 num_classes: Optional[Sequence[str]] = None,
+                 label_words: Optional[Union[Sequence[str], Mapping[str, str]]] = None,
+                 prefix: Optional[str] = " ",
+                 multi_token_handler: Optional[str] = "first",
+                 post_log_softmax: Optional[bool] = True,
+                 ):
+        super().__init__(tokenizer=tokenizer, num_classes=num_classes, classes=classes)
+        self.prefix = prefix
+        self.multi_token_handler = multi_token_handler
+        self.label_words = label_words
+        self.post_log_softmax = post_log_softmax
+
+    def on_label_words_set(self):
+        super().on_label_words_set()
+        self.label_words = self.add_prefix(self.label_words, self.prefix)
+
+        # TODO should Verbalizer base class has label_words property and setter?
+        # it don't have label_words init argument or label words from_file option at all
+
+        self.generate_parameters()
+
+    @staticmethod
+    def add_prefix(label_words, prefix):
+        r"""Add prefix to label words. For example, if a label words is in the middle of a template,
+        the prefix should be ``' '``.
+
+        Args:
+            label_words (:obj:`Union[Sequence[str], Mapping[str, str]]`, optional): The label words that are projected by the labels.
+            prefix (:obj:`str`, optional): The prefix string of the verbalizer.
+
+        Returns:
+            :obj:`Sequence[str]`: New label words with prefix.
+        """
+        new_label_words = []
+        if isinstance(label_words[0], str):
+            label_words = [[w] for w in label_words]  # wrapped it to a list of list of label words.
+
+        for label_words_per_label in label_words:
+            new_label_words_per_label = []
+            for word in label_words_per_label:
+                if word.startswith("<!>"):
+                    new_label_words_per_label.append(word.split("<!>")[1])
+                else:
+                    new_label_words_per_label.append(prefix + word)
+            new_label_words.append(new_label_words_per_label)
+        return new_label_words
+
+    def generate_parameters(self) -> List:
+        r"""In basic manual template, the parameters are generated from label words directly.
+        In this implementation, the label_words should not be tokenized into more than one token.
+        """
+        all_ids = []
+        for words_per_label in self.label_words:
+            ids_per_label = []
+            for word in words_per_label:
+                ids = self.tokenizer.encode(word)
+                ids_per_label.append(ids.ids)
+            all_ids.append(ids_per_label)
+
+        max_len = max([max([len(ids) for ids in ids_per_label]) for ids_per_label in all_ids])
+        max_num_label_words = max([len(ids_per_label) for ids_per_label in all_ids])
+        words_ids_mask = ops.zeros((max_num_label_words, max_len))
+        words_ids_mask = [[[1] * len(ids) + [0] * (max_len - len(ids)) for ids in ids_per_label]
+                          + [[0] * max_len] * (max_num_label_words - len(ids_per_label))
+                          for ids_per_label in all_ids]
+        words_ids = [[ids + [0] * (max_len - len(ids)) for ids in ids_per_label]
+                     + [[0] * max_len] * (max_num_label_words - len(ids_per_label))
+                     for ids_per_label in all_ids]
+
+        words_ids_tensor = Tensor(words_ids)
+        words_ids_mask = Tensor(words_ids_mask)
+        self.label_words_ids = Parameter(words_ids_tensor, requires_grad=False)
+        self.words_ids_mask = Parameter(words_ids_mask, requires_grad=False)  # A 3-d mask
+        self.label_words_mask = Parameter(ops.clamp(words_ids_mask.sum(axis=-1), max=1), requires_grad=False)
+
+    def project(self,
+                logits: Tensor,
+                **kwargs,
+                ) -> Tensor:
+        r"""
+        Project the labels, the return value is the normalized (sum to 1) probs of label words.
+
+        Args:
+            logits (:obj:`Tensor`): The original logits of label words.
+
+        Returns:
+            :obj:`Tensor`: The normalized logits of label words
+        """
+
+        label_words_logits = logits[:, self.label_words_ids]
+        label_words_logits = self.handle_multi_token(label_words_logits, self.words_ids_mask)
+        # 修改
+        # label_words_logits -= 10000 * (1 - self.label_words_mask)
+        label_words_logits -= 10000 * ops.sub(1, self.label_words_mask)
+        return label_words_logits
+
+    def process_logits(self, logits: Tensor, **kwargs):
+        r"""A whole framework to process the original logits over the vocabulary, which contains four steps:
+
+        (1) Project the logits into logits of label words
+
+        if self.post_log_softmax is True:
+
+            (2) Normalize over all label words
+
+            (3) Calibrate (optional)
+
+        (4) Aggregate (for multiple label words)
+
+        Args:
+            logits (:obj:`Tensor`): The original logits.
+
+        Returns:
+            (:obj:`Tensor`): The final processed logits over the labels (classes).
+        """
+        # project
+        label_words_logits = self.project(logits,
+                                          **kwargs)  # Output: (batch_size, num_classes) or  (batch_size, num_classes, num_label_words_per_label)
+
+        if self.post_log_softmax:
+            # normalize
+            label_words_probs = self.normalize(label_words_logits)
+
+            # calibrate
+            if hasattr(self, "_calibrate_logits") and self._calibrate_logits is not None:
+                label_words_probs = self.calibrate(label_words_probs=label_words_probs)
+
+            # convert to logits
+            label_words_logits = ops.log(label_words_probs + 1e-15)
+
+        # aggregate
+        label_logits = self.aggregate(label_words_logits)
+        return label_logits
+
+    def normalize(self, logits: Tensor) -> Tensor:
+        """
+        Given logits regarding the entire vocabulary, return the probs over the label words set.
+
+        Args:
+            logits (:obj:`Tensor`): The logits over the entire vocabulary.
+
+        Returns:
+            :obj:`Tensor`: The logits over the label words set.
+
+        """
+        batch_size = logits.shape[0]
+        return ops.softmax(logits.reshape(batch_size, -1), axis=-1).reshape(*logits.shape)
+
+    def aggregate(self, label_words_logits: Tensor) -> Tensor:
+        r"""Use weight to aggregate the logits of label words.
+
+        Args:
+            label_words_logits(:obj:`Tensor`): The logits of the label words.
+
+        Returns:
+            :obj:`Tensor`: The aggregated logits from the label words.
+        """
+        label_words_logits = (label_words_logits * self.label_words_mask).sum(-1) / self.label_words_mask.sum(-1)
+        return label_words_logits
+
+    def calibrate(self, label_words_probs: Tensor, **kwargs) -> Tensor:
+        r"""
+
+        Args:
+            label_words_probs (:obj:`Tensor`): The probability distribution of the label words with the shape of [``batch_size``, ``num_classes``, ``num_label_words_per_class``]
+
+        Returns:
+            :obj:`Tensor`: The calibrated probability of label words.
+        """
+        shape = label_words_probs.shape
+        assert self._calibrate_logits.dim() == 1, "self._calibrate_logits are not 1-d tensor"
+        calibrate_label_words_probs = self.normalize(self.project(self._calibrate_logits.unsqueeze(0), **kwargs))
+        assert calibrate_label_words_probs.shape[1:] == label_words_probs.shape[1:] \
+               and calibrate_label_words_probs.shape[0] == 1, "shape not match"
+        label_words_probs /= (calibrate_label_words_probs + 1e-15)
+        # normalize # TODO Test the performance
+        norm = label_words_probs.reshape(shape[0], -1).sum(dim=-1,
+                                                           keepdim=True)  # TODO Test the performance of detaching()
+        label_words_probs = label_words_probs.reshape(shape[0], -1) / norm
+        label_words_probs = label_words_probs.reshape(*shape)
+        return label_words_probs
