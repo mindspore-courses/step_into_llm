a8e73754a0b6aafd92016909506cc473e54bf268
nanzhaogang <42226610+nanzhaogang@users.noreply.github.com>
2023-08-16 15:17:04 +0800

    Add files via upload

--- 5.Parallel/operator_parallel/run.sh
+++ 5.Parallel/operator_parallel/run.sh
@@ -7,4 +7,9 @@ echo "For example: bash run.sh 8"
 echo "This example is expected to run on the Ascend environment."
 echo "=============================================================================================================="
 
-mpirun -n 2 python train.py
+if ! [[ $1 =~ ^[1-8]$ ]]; then
+  echo "[error]Please run the script as: bash run.sh 2"
+  exit 0
+fi
+
+mpirun -n $1 python train.py $1
