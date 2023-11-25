031237fa5b1be8303a0e79ebb6cd1ec8b0a97f75
lvyufeng <lvyufeng@cqu.edu.cn>
2023-11-25 16:13:15 +0800

    update codes and slides of decoding

---
+++ Season2.step_into_llm/03.Decoding/greedy_search.py
@@ -0,0 +1,15 @@
+from mindnlp.transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+# add the EOS token as PAD token to avoid warnings
+model = GPT2LMHeadModel.from_pretrained("gpt2", pad_token_id=tokenizer.eos_token_id)
+
+# encode context the generation is conditioned on
+input_ids = tokenizer.encode('I enjoy walking with my cute dog', return_tensors='ms')
+
+# generate text until the output length (which includes the context length) reaches 50
+greedy_output = model.generate(input_ids, max_length=50)
+
+print("Output:\n" + 100 * '-')
+print(tokenizer.decode(greedy_output[0], skip_special_tokens=True))
---
+++ Season2.step_into_llm/03.Decoding/top_p.py
@@ -0,0 +1,24 @@
+import mindspore
+from mindnlp.transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+# add the EOS token as PAD token to avoid warnings
+model = GPT2LMHeadModel.from_pretrained("gpt2", pad_token_id=tokenizer.eos_token_id)
+
+# encode context the generation is conditioned on
+input_ids = tokenizer.encode('I enjoy walking with my cute dog', return_tensors='ms')
+
+mindspore.set_seed(0)
+
+# deactivate top_k sampling and sample only from 92% most likely words
+sample_output = model.generate(
+    input_ids, 
+    do_sample=True, 
+    max_length=50, 
+    top_p=0.92, 
+    top_k=0
+)
+
+print("Output:\n" + 100 * '-')
+print(tokenizer.decode(sample_output[0], skip_special_tokens=True))
