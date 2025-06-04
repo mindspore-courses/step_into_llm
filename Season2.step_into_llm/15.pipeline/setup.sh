ba2ca62a98520f2610364e18c09c81fa85da3da1
Sophia-0224 <2920904163@qq.com>
2025-06-04 21:05:10 +0800

    Update setup.sh

--- Season2.step_into_llm/15.pipeline/setup.sh
+++ Season2.step_into_llm/15.pipeline/setup.sh
@@ -1,2 +1,2 @@
-pip install https://ms-release.obs.cn-north-4.myhuaweicloud.com/2.4.10/MindSpore/unified/aarch64/mindspore-2.4.10-cp39-cp39-linux_aarch64.whl --trusted-host ms-release.obs.cn-north-4.myhuaweicloud.com -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install https://ms-release.obs.cn-north-4.myhuaweicloud.com/2.6.0/MindSpore/unified/aarch64/mindspore-2.6.0-cp39-cp39-linux_aarch64.whl --trusted-host ms-release.obs.cn-north-4.myhuaweicloud.com -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install mindnlp librosa pytesseract
